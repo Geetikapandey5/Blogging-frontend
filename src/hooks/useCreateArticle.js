e8ef4441fc8bfde3b8d75bdf96b99d46bebb0470
Geetikapandey5 <geetikapandey5082001@gmail.com>
2024-07-24 22:05:37 +0530

    done

--- src/hooks/useCreateArticle.js
+++ src/hooks/useCreateArticle.js
@@ -7,7 +7,7 @@ const creteArticle = async (values) => {
     console.log("creteArticle", { values });
 
     const { data } = await axios.post(
-        `http://localhost:3001/api/articles`,
+        `https://blog-website-backend-risd.onrender.com/api/articles`,
         { article: {...values.values} }
       );
 
--- src/hooks/useTagsQuery.js
+++ src/hooks/useTagsQuery.js
@@ -3,7 +3,7 @@ import React from "react";
 import axios from 'axios';
 
 const getAllTags = async () => {
-  const {data} = await axios.get(`http://localhost:3001/api/tags`);
+  const {data} = await axios.get(`https://blog-website-backend-risd.onrender.com/api/tags`);
 
 //   console.log("getCurrentUser", { data });
 
